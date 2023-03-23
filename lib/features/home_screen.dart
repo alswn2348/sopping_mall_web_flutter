a5ffa252a8d890ac3bbd4266e2b94de93b77ba01
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-23 23:24:20 +0900

    style : add image

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -14,8 +14,15 @@ class Homescreen extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 200, vertical: 50),
           child: Column(
-            children: const [
-              MainNavigatorBar(),
+            children: [
+              const MainNavigatorBar(),
+              Gaps.v10,
+              Image.network(
+                "https://static.wixstatic.com/media/8c1bd9_9355dea12dde4adda42b5bfc79e23126~mv2.jpg/v1/crop/x_0,y_53,w_2362,h_1555/fill/w_1261,h_830,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/small.jpg",
+                width: 1700,
+                height: 1000,
+                fit: BoxFit.fill,
+              ),
             ],
           ),
         ),
