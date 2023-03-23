de18cca6dcaa43646e480fccb49d98f967bc0171
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-23 23:23:58 +0900

    feat : create main navigator bar

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -1,6 +1,7 @@
+import 'package:e_commerce_flutter/constants/gaps.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'main_navigator_bar.dart';
 
 class Homescreen extends StatelessWidget {
   static const String routeName = '/';
@@ -9,8 +10,15 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sopping Mall'),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 200, vertical: 50),
+          child: Column(
+            children: const [
+              MainNavigatorBar(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/main_navigator_bar.dart
@@ -0,0 +1,73 @@
+import 'package:e_commerce_flutter/constants/gaps.dart';
+import 'package:e_commerce_flutter/constants/sizes.dart';
+import 'package:e_commerce_flutter/features/authentication/login_form_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainNavigatorBar extends StatefulWidget {
+  const MainNavigatorBar({
+    super.key,
+  });
+
+  @override
+  State<MainNavigatorBar> createState() => _MainNavigatorBarState();
+}
+
+class _MainNavigatorBarState extends State<MainNavigatorBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('SOHPPING MALL', style: Theme.of(context).textTheme.headlineLarge),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                Text(
+                  'HOME',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Gaps.h32,
+                Text(
+                  'SHOP',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Gaps.h32,
+                Text(
+                  'COLLECTIONS',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Gaps.h32,
+                Text(
+                  'ABOUT',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Gaps.h32,
+                Text(
+                  'More',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                GestureDetector(onTap: _onLoginTap, child: const Text('login')),
+                Gaps.h32,
+                const Icon(
+                  Icons.shop,
+                  size: Sizes.size36,
+                ),
+              ],
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  void _onLoginTap() {
+    Navigator.of(context).pushNamed(LoginFormScreen.routeName);
+  }
+}
