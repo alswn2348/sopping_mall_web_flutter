d6f0429b42831c70c3f1216925aaf4848601cc5b
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-24 18:48:35 +0900

    feat : add login button function

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:e_commerce_flutter/constants/gaps.dart';
+import 'package:e_commerce_flutter/features/authentication/sign_up_screen.dart';
 import 'package:e_commerce_flutter/features/home_screen.dart';
 import 'package:flutter/material.dart';
+
 import 'widgets/form_button.dart';
+import 'package:http/http.dart' as http;
 
 class LoginFormScreen extends StatefulWidget {
   static const String routeName = "/login_form";
@@ -16,15 +21,21 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
 
   Map<String, String> formData = {};
 
-  void _onSubmitTap() {
+  void _onSubmitTap() async {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(
-              builder: (context) => const Homescreen(),
-            ),
-            (route) => false);
+
+        var response = await http.post(Uri.parse("http://localhost:8080/login"),
+            body: jsonEncode(formData));
+        if (response.statusCode == 200) {
+          Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                  builder: (BuildContext context) => const Homescreen()),
+              (Route<dynamic> route) => false);
+        } else {
+          setState(() {});
+        }
       }
     }
   }
@@ -32,68 +43,97 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 200),
-        alignment: Alignment.center,
-        child: Form(
-            key: _formKey,
-            child: SizedBox(
-              width: 500,
-              child: Column(
-                children: [
-                  Text(
-                    "Login",
-                    style: Theme.of(context).textTheme.displayLarge,
-                  ),
-                  Gaps.v28,
-                  Text(
-                    '이 사이트가 처음이신가요? 회원가입',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value != null && value.isEmpty) {
-                        return "Plase write your email";
-                      }
-                      return null;
-                    },
-                    onSaved: (newValue) {
-                      if (newValue != null) {
-                        formData['email'] = newValue;
-                      }
-                    },
-                    decoration: const InputDecoration(hintText: 'Email'),
-                  ),
-                  Gaps.v16,
-                  TextFormField(
-                    obscureText: true,
-                    validator: (value) {
-                      if (value != null && value.isEmpty) {
-                        return "Plase write your password";
-                      }
-                      return null;
-                    },
-                    onSaved: (newValue) {
-                      if (newValue != null) {
-                        formData['password'] = newValue;
-                      }
-                    },
-                    decoration: const InputDecoration(hintText: 'Password'),
-                  ),
-                  Gaps.v28,
-                  GestureDetector(
-                    onTap: _onSubmitTap,
-                    child: const FormButton(
-                      disabled: false,
-                      title: 'Log in',
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 200),
+          alignment: Alignment.center,
+          child: Form(
+              key: _formKey,
+              child: SizedBox(
+                width: 350,
+                child: Column(
+                  children: [
+                    Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.displayLarge,
                     ),
-                  ),
-                  Gaps.v56,
-                  const Text('비밀번호를 잊어 버렸나요? ')
-                ],
-              ),
-            )),
+                    Gaps.v28,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '이 사이트가 처음이신가요? ',
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        GestureDetector(
+                          onTap: _onSignUpTap,
+                          child: Text(
+                            '회원가입',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 110, 9, 9),
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium
+                                  ?.fontSize,
+                              fontWeight: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium
+                                  ?.fontWeight,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Gaps.v32,
+                    TextFormField(
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Plase write your email";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        if (newValue != null) {
+                          formData['account'] = newValue;
+                        }
+                      },
+                      decoration: const InputDecoration(hintText: 'Email'),
+                    ),
+                    Gaps.v16,
+                    TextFormField(
+                      obscureText: true,
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Plase write your password";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        if (newValue != null) {
+                          formData['password'] = newValue;
+                        }
+                      },
+                      decoration: const InputDecoration(hintText: 'Password'),
+                    ),
+                    Gaps.v28,
+                    GestureDetector(
+                      onTap: _onSubmitTap,
+                      child: const FormButton(
+                        disabled: false,
+                        title: 'Log in',
+                      ),
+                    ),
+                    Gaps.v56,
+                    const Text('비밀번호를 잊어 버렸나요? ')
+                  ],
+                ),
+              )),
+        ),
       ),
     );
+  }
+
+  void _onSignUpTap() {
+    Navigator.of(context).pushNamed(SignUpScreen.routeName);
   }
 }
