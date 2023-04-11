268c9d8ef0473091b1d76662b89be620447b449b
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-04-11 16:33:29 +0900

    feat : product list
    제품 리스트 추가
    어드민패널 색 추가

---
+++ lib/constants/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AdminColor {
+  static const primary = Color(0xFF2697FF);
+  static const secondary = Color(0xFF2A2D3E);
+  static const bg = Color(0xFF212332);
+}
---
+++ lib/features/admin/view/widgets/product_list.dart
@@ -0,0 +1,103 @@
+import 'package:data_table_2/data_table_2.dart';
+import 'package:e_commerce_flutter/constants/color.dart';
+import 'package:e_commerce_flutter/constants/sizes.dart';
+import 'package:e_commerce_flutter/features/admin/view/widgets/add_button.dart';
+import 'package:e_commerce_flutter/features/shop/logic/models/product.dart';
+import 'package:e_commerce_flutter/features/shop/logic/view_model/product_post_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product(imgPath: "imgPath", name: "name", price: 500)
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    initProducts();
+  }
+
+  void initProducts() async {
+    products = await context.read<ProductPostViewModel>().updateItem();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(Sizes.size40),
+      decoration: const BoxDecoration(
+        color: AdminColor.secondary,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "Product List",
+                style: TextStyle(color: Colors.white),
+              ),
+              AddButton(),
+            ],
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 800,
+            child: DataTable2(
+              columns: const [
+                DataColumn(
+                  label: Text(
+                    "Product Name",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    "Date",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+              rows: List.generate(
+                products.length,
+                (index) => recentFileDataRow(products[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+DataRow recentFileDataRow(Product product) {
+  return DataRow(
+    cells: [
+      DataCell(
+        Text(
+          product.name,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      DataCell(
+        Text(
+          product.price.toString(),
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    ],
+  );
+}
